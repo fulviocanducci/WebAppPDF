ea8e053c410bb9f68160753c35230ec4389b29fc
Fúlvio <fulviocanducci@hotmail.com>
2022-10-27 11:26:08 -0300

    Add project files.

---
+++ Pdf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pdf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
